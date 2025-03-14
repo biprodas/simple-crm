00f980c301882cfac14dda68948fe29c7642fee7
Biprodas Roy <biprodas.cse@gmail.com>
2025-03-15 01:43:37 +0600

    frontend setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "~/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="p-3">
+      <h2 className="text-red-700">Hello CRM</h2>
+      <Button>Click me</Button>
+    </div>
+  );
+}
